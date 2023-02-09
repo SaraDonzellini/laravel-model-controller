b1a38b24faa2d83c673f41fba4f5278dcd62c758
SaraDonzellini <saradonzie@gmail.com>
2023-02-09 14:05:12 +0100

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,35 @@
+@extends('layouts/app')
+
+@section('main-content')
+  <div class="container">
+    <div class="row">
+      <div class="col-12">
+        <h1>
+          Movies
+        </h1>
+      </div>
+    </div>
+    <div class="row">
+      @foreach ($movies as $movie)
+      {{-- @dd($movie) --}}
+      <div class="col-3">
+        <div class="card">
+          <img src="" alt="{{ $movie->title }}" class="img-fluid">
+          <h2>
+            {{ $movie->title }}
+          </h2>
+          <h4>
+            {{ $movie->original_title }}
+          </h4>
+          <p>
+            {{ $movie->date }}
+          </p>
+        </div>
+      </div>
+      
+      @endforeach
+    </div>
+  </div>
+
+
+@endsection
